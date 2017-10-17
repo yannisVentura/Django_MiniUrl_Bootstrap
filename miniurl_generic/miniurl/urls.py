46e202fb6740c182e2faf2cea344d9466744faf8
Ventura <yventura@RCFNET.rockwellcollins.com>
2017-10-17 13:17:26 +0200

    Modify home page url

--- miniurl_generic/miniurl/urls.py
+++ miniurl_generic/miniurl/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^mini_url/', include('url.urls')),
+    url(r'', include('url.urls')),
 ]
--- miniurl_generic/url/urls.py
+++ miniurl_generic/url/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 app_name = 'mini_url'
 urlpatterns = [
-    url(r'^home/$', views.ListRedirection_view.as_view(), name='home'),
+    url(r'^$', views.ListRedirection_view.as_view(), name='home'),
     url(r'^redirect/(?P<code>\d+)/$', views.redirected_view, name='redirect'),
-    url(r'^acces/(?P<code>\d+)/$', views.access_url, name='redirect'),
+    url(r'^mini_url/acces/(?P<code>\d+)/$', views.access_url, name='redirect'),
     url(r'^about/$', views.FAQView.as_view(), name='about'),
-    url(r'^create_miniurl/$', views.CreateUrlView.as_view(), name="create-miniurl"),
+    url(r'create_miniurl/$', views.CreateUrlView.as_view(), name="create-miniurl"),
 ]
